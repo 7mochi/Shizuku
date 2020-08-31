08364ae45d365421a229a80c77924ba71c2f3a55
FlyingCat-X <flyingcatdm@gmail.com>
2020-08-31 18:26:51 -0500

    Version 2020.08.31 - Updating Shizuku Version

--- src/main/java/pe/flyingcat/shizukubot/util/ApplicationInfo.java
+++ src/main/java/pe/flyingcat/shizukubot/util/ApplicationInfo.java
@@ -6,7 +6,7 @@ package pe.flyingcat.shizukubot.util;
  */
 public final class ApplicationInfo {
 
-    public static final String VERSION = "2019.08.05";
+    public static final String VERSION = "2020.08.31";
     public static final String APP = "Shizuku" + " v" + VERSION;
 
     private ApplicationInfo() {
