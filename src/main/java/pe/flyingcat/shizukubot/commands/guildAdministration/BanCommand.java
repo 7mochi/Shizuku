840588a4b851adb0a8fca51c095328b914d4a009
FlyingCat-X <flyingcatdm@gmail.com>
2020-09-02 18:52:34 -0500

    Version 2020.09.02 - Removing redundant code

--- src/main/java/pe/flyingcat/shizukubot/commands/guildAdministration/BanCommand.java
+++ src/main/java/pe/flyingcat/shizukubot/commands/guildAdministration/BanCommand.java
@@ -12,6 +12,7 @@ import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.exceptions.HierarchyException;
+import net.dv8tion.jda.api.exceptions.InsufficientPermissionException;
 import pe.flyingcat.shizukubot.commands.Command;
 import pe.flyingcat.shizukubot.util.RegExp;
 
@@ -48,15 +49,15 @@ public class BanCommand extends Command {
                 sendMessage(e, builder.build());
             } else {
                 Member author = e.getMessage().getMember();
-                if (author.hasPermission(Permission.BAN_MEMBERS)) {
-                    List<Member> members = e.getMessage().getMentionedMembers();
-                    // Author have permission but didn't tag user
-                    if (members.isEmpty()) {
-                        MessageBuilder builder = new MessageBuilder();
-                        builder.append(e.getAuthor());
-                        builder.append(multiLang.getMessage("APP_BAN_COMM_EMPTY"));
-                        sendMessage(e, builder.build());
-                    } else {
+                List<Member> members = e.getMessage().getMentionedMembers();
+                // Author didn't tag user
+                if (members.isEmpty()) {
+                    MessageBuilder builder = new MessageBuilder();
+                    builder.append(e.getAuthor());
+                    builder.append(multiLang.getMessage("APP_BAN_COMM_EMPTY"));
+                    sendMessage(e, builder.build());
+                } else {
+                    if (author.hasPermission(Permission.BAN_MEMBERS)) {
                         matcher = Pattern.compile(RegExp.TEXT_BETWEEN_QUOTES).matcher(argsStr);
                         List<String> allMatches = new ArrayList<>();
                         while (matcher.find()) {
@@ -83,10 +84,14 @@ public class BanCommand extends Command {
                                     builder.append(members.get(0).getUser().getName());
                                     builder.append(multiLang.getMessage("APP_BAN_COMM_SUCCESS_2"));
                                     sendMessage(e, builder.build());
-                                } catch (HierarchyException ex) {
+                                } catch (Exception ex) {
                                     MessageBuilder builder = new MessageBuilder();
                                     builder.append(e.getAuthor());
-                                    builder.append(multiLang.getMessage("APP_BAN_COMM_HIGHER_ROL"));
+                                    if (ex instanceof HierarchyException) {
+                                        builder.append(multiLang.getMessage("APP_BAN_COMM_HIGHER_ROL"));
+                                    } else if (ex instanceof InsufficientPermissionException) {
+                                        builder.append(multiLang.getMessage("APP_BAN_COMM_INSUFF_PERMS"));
+                                    }
                                     sendMessage(e, builder.build());
                                 }
                             }
@@ -96,16 +101,8 @@ public class BanCommand extends Command {
                             builder.append(multiLang.getMessage("APP_BAN_COMM_DMD_ERR"));
                             sendMessage(e, builder.build());
                         }
-                    }
-                } else {
-                    List<Member> members = e.getMessage().getMentionedMembers();
-                    // Author don't have permission and didn't tag user
-                    if (members.isEmpty()) {
-                        MessageBuilder builder = new MessageBuilder();
-                        builder.append(e.getAuthor());
-                        builder.append(multiLang.getMessage("APP_BAN_COMM_EMPTY"));
-                        sendMessage(e, builder.build());
-                    } else { // Author don't have permission
+                    } else {
+                        // Author doesn't have permission 
                         MessageBuilder builder = new MessageBuilder();
                         builder.append(e.getAuthor());
                         builder.append(multiLang.getMessage("APP_BAN_COMM_CANT"));
